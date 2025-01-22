7a9329a0837d700ec8ffcfe17a82b7c62bf08b9d
DSnake0 <revilwhoami0098@gmail.com>
2025-01-22 18:12:12 +0500

    Add npm crash course file & modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
                     {/* <LearnEvent /> */}
                     {/* <LearnLiftingStateUp myClick={getData} /> */}
                     {/* <LearnState /> */}
-                    <CounterApp />
+                    {/* <CounterApp /> */}
                 </div>
             </div>
         </>
