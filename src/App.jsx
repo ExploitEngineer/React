155122200f56c80b7c51bd621d4c9f012590a291
DSnake0 <revilwhoami0098@gmail.com>
2025-01-28 13:52:37 +0500

    minor changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import LearnComponent from './components/LearnComponent'
-import LearnJsx from './components/LearnJsx'
-import LearnProps from './components/LearnProps'
-import LearnEvent from './components/LearnEvent'
-import LearnLiftingStateUp from './components/LearnLiftingStateUp'
-import LearnState from './components/LearnState'
-import CounterApp from './components/CounterApp'
-import LearnConditionalRendering from './components/LearnConditionalRendering'
-import Leranmap from './components/LearnMap'
-import LearnInlineCss from './components/LearnInlineCss'
-import LearnForm from './components/LearnForm'
-import UseState from './components/Hooks/UseState'
-import TodoApplication from './components/Todo_Application/Maintodo'
+import React, { useState } from 'react';
+import LearnComponent from './components/LearnComponent';
+import LearnJsx from './components/LearnJsx';
+import LearnProps from './components/LearnProps';
+import LearnEvent from './components/LearnEvent';
+import LearnLiftingStateUp from './components/LearnLiftingStateUp';
+import LearnState from './components/LearnState';
+import CounterApp from './components/CounterApp';
+import LearnConditionalRendering from './components/LearnConditionalRendering';
+import Leranmap from './components/LearnMap';
+import LearnInlineCss from './components/LearnInlineCss';
+import LearnForm from './components/LearnForm';
+import UseState from './components/Hooks/UseState';
+import TodoApplication from './components/Todo_Application/Maintodo';
+import UseEffect from './components/Hooks/UseEffect';
 
 const App = () => {
 
@@ -19,6 +20,8 @@ const App = () => {
     const getData = (data) => {
         console.log(data);
     }
+
+    const [isVisible, setVisible] = useState(true);
 
     return (
         <>
@@ -35,8 +38,10 @@ const App = () => {
                     {/* <Leranmap /> */}
                     {/* <LearnInlineCss /> */}
                     {/* <LearnForm /> */}
-                    <UseState />
+                    {/* <UseState /> */}
                     {/* <TodoApplication /> */}
+                    {isVisible ? <UseEffect /> : <></>}
+                    <button className='text-white font-semibold font-mono px-3 py-1 rounded-xl bg-purple-600' onClick={() => setVisible(!isVisible)}>Cleanup</button>
                 </div>
             </div>
         </>
