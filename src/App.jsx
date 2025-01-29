95fa04ccc79b6e518b4c2a3ba140afd0507c429d
DSnake0 <revilwhoami0098@gmail.com>
2025-01-29 14:33:41 +0500

    Add useCallback.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import LearnComponent from './components/LearnComponent';
 import LearnJsx from './components/LearnJsx';
 import LearnProps from './components/LearnProps';
@@ -15,6 +15,7 @@ import TodoApplication from './components/Todo_Application/Maintodo';
 import UseEffect from './components/Hooks/UseEffect';
 import UseMemo from './components/Hooks/UseMemo';
 import UseRef from './components/Hooks/UseRef';
+import UseCallback from './components/Hooks/UseCallback';
 
 const App = () => {
 
@@ -24,6 +25,12 @@ const App = () => {
     }
 
     const [isVisible, setVisible] = useState(true);
+
+    const [add, setAdd] = useState(0);
+
+    const Learning = useCallback(() => {
+        // some operations
+    }, [])
 
     return (
         <>
@@ -46,6 +53,9 @@ const App = () => {
                     <button className='text-white font-semibold font-mono px-3 py-1 rounded-xl bg-purple-600' onClick={() => setVisible(!isVisible)}>Cleanup</button> */}
                     {/* <UseMemo /> */}
                     {/* <UseRef /> */}
+                    {/* <UseCallback Learning={Learning} />
+                    <h1 className='text-white font-semibold text-xl font-mono text-center mb-3'>{add}</h1>
+                    <button type='button' className='px-3 py-1 bg-rose-600 text-white font-semibold font-mono rounded-xl' onClick={() => setAdd(add + 1)}>Addition {add}</button> */}
                 </div>
             </div>
         </>
---
+++ src/components/Hooks/UseCallback.jsx
@@ -0,0 +1,17 @@
+const UseCallback = ({ Learning }) => {
+
+    // What is useCallback and why we use it ?
+    // How to use useCallback with example ?
+
+    function useCallbkFun() {
+        console.log("useCallback Component");
+    }
+    useCallbkFun();
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default UseCallback;
