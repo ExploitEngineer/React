d4697e0dee8877963913ddc1a109d4d80cb66ab0
ExploitEngineer <revilwhoami0098@gmail.com>
2025-04-01 17:16:07 +0500

    Add complete React & redux folder

---
+++ Complete_React_Redux/src/App.jsx
@@ -0,0 +1,11 @@
+import ClassBasedComponent from './components/Class_based_component';
+
+const App = () => {
+    return (
+        <div className="w-full min-h-screen flex items-center justify-center bg-zinc-800">
+            <ClassBasedComponent />
+        </div>
+    )
+}
+
+export default App;
---
+++ Complete_React_Redux/src/components/Class_based_component.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+
+class ClassBasedComponent extends Component {
+
+    render() {
+        return (
+            <>
+                <h1 className="text-white font-semibold font-mono text-2xl">Class Based Component</h1>
+            </>
+        );
+    };
+};
+
+export default ClassBasedComponent;
