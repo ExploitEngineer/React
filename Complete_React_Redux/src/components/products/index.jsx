ef589fb4331a0e48438789d4203c007bc4904dc5
ExploitEngineer <revilwhoami0098@gmail.com>
2025-04-03 21:58:41 +0500

    Add Conditional Statements

--- Complete_React_Redux/src/components/products/index.jsx
+++ Complete_React_Redux/src/components/products/index.jsx
@@ -3,10 +3,20 @@ import './style.css';
 
 const productList = ({ name, city, data }) => {
 
+    const flag = true;
+
+    // function renderTextBlock(getFlag) {
+    //     return getFlag ? <h4>Name is {name}, he/she belongs to this city {city}</h4> : <h4>Hello World!</h4>
+    // };
+
+    const renderTextBlock = flag ? <h4>Name is {name}, he/she belongs to this city {city}</h4> : <h4>Hello World!</h4>;
+
+
     return (
         <div className='text-center text-white font-semibold text-2xl font-mono space-y-4'>
             <h3 className='title'>ECommerce Project</h3>
-            <h4>Name is {name}, he/she belongs to this city {city}</h4>
+            {renderTextBlock}
+
             {/* <ProductItem /> */}
             <ul>
                 {
@@ -19,6 +29,6 @@ const productList = ({ name, city, data }) => {
             </ul>
         </div>
     )
-}
+};
 
 export default productList;
