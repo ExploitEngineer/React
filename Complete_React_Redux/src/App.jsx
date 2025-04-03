d1f9617e6decb84658b9167a8214f38247a4fb13
ExploitEngineer <revilwhoami0098@gmail.com>
2025-04-03 22:07:37 +0500

    Add states in classes

--- Complete_React_Redux/src/App.jsx
+++ Complete_React_Redux/src/App.jsx
@@ -9,9 +9,9 @@ const App = () => {
     return (
         <div className="w-full min-h-screen flex items-center justify-center bg-zinc-800">
             <div className='flex flex-col justify-center'>
-                {/* <ClassBasedComponent />
-                <FunctionalComponent /> */}
-                <ProductList data={dummyProducts} name='rafay' city='Islamabad' />
+                <ClassBasedComponent />
+                {/* <FunctionalComponent /> */}
+                {/* <ProductList data={dummyProducts} name='rafay' city='Islamabad' /> */}
             </div>
         </div>
     )
--- Complete_React_Redux/src/components/Class_based_component.jsx
+++ Complete_React_Redux/src/components/Class_based_component.jsx
@@ -3,10 +3,27 @@ import { Component } from "react";
 
 class ClassBasedComponent extends Component {
 
+    // state
+    state = {
+        showText: false
+    };
+
+    handleClick = () => {
+        this.setState({
+            showText: !this.state.showText
+        })
+    };
+
     render() {
+
+        console.log(this.state);
+
         return (
             <>
-                <h1 className="text-white font-semibold font-mono text-2xl">Class Based Component</h1>
+                {
+                    this.state.showText ? <h1 className="text-white font-semibold font-mono text-2xl">Class Based Component</h1> : null
+                }
+                <button className="btn btn-secondary" onClick={this.handleClick}>Toggle Text</button>
             </>
         );
     };
