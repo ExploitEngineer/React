c807e2f4a918618f10f26df406823fb15aa29bd6
ExploitEngineer <revilwhoami0098@gmail.com>
2025-04-05 13:49:46 +0500

    Add useLocation hook

--- Complete_React_Redux/src/components/reactRouter/Home.jsx
+++ Complete_React_Redux/src/components/reactRouter/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Home = () => {
+
+    const location = useLocation();
+    const { state: { getId } } = location;
+
     return (
         <div className='text-center font-semibold font-mono'>
-            <h1 className='text-xl'>Home</h1>
+            <h1 className='text-xl'>Home and id is {getId}</h1>
             <div className='space-y-3 mt-5 flex flex-col'>
                 <Link className='btn btn-primary' to='/about'>About</Link>
                 <Link className='btn btn-secondary' to='/contact'>Contact</Link>
--- Complete_React_Redux/src/components/reactRouter/useNavigation/UseNavigation.jsx
+++ Complete_React_Redux/src/components/reactRouter/useNavigation/UseNavigation.jsx
@@ -2,11 +2,12 @@ import { useNavigate } from "react-router-dom";
 
 export default function UseNavigation() {
 
+    const id = 69;
     const navigate = useNavigate();
 
     return (
         <div className="flex flex-col justify-center items-center gap-5">
-            <button className="btn btn-primary" onClick={() => navigate('/')}>Home</button>
+            <button className="btn btn-primary" onClick={() => navigate('/', { state: { getId: id } })}>Home</button>
             <button className="btn btn-secondary" onClick={() => navigate('/about')}>About</button>
             <button className="btn btn-warning" onClick={() => navigate('/contact')}>Contact</button>
         </div>
