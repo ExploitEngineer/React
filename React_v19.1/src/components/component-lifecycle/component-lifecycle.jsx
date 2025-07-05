69fdfb6869a15a2a2531fd8e6fd9f7703e90b567
ExploitEngineer <revilwhoami0098@gmail.com>
2025-07-06 04:42:20 +0500

    feat: add component lifecycle

---
+++ React_v19.1/src/components/component-lifecycle/component-lifecycle.jsx
@@ -0,0 +1,9 @@
+export function ComponentLifeCycle() {
+  return (
+    <div className="flex flex-col items-center !text-white font-medium">
+      <p>Mount (appear)</p>
+      <p>Update (change)</p>
+      <p>Unmount (disappear)</p>
+    </div>
+  );
+}
