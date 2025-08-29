64378be02551f4b5624731bc744eea650f582278
ExploitEngineer <revilwhoami0098@gmail.com>
2025-08-29 17:01:52 +0500

    feat: implement simple counter using zustand

---
+++ zustand/src/App.tsx
@@ -0,0 +1,30 @@
+import { create } from "zustand";
+import { Button } from "./components/ui/button.tsx";
+
+const useStore = create<{
+  count: number;
+  inc: () => void;
+  dec: () => void;
+}>((set) => ({
+  count: 0,
+  inc: () => set((state) => ({ count: state.count + 1 })),
+  dec: () => set((state) => ({ count: state.count - 1 })),
+}));
+
+function App() {
+  const store = useStore();
+  return (
+    <>
+      <Button onClick={store.inc}>+</Button>
+      <Count />
+      <Button onClick={store.dec}>-</Button>
+    </>
+  );
+}
+
+function Count() {
+  const store = useStore();
+  return <>{store.count}</>;
+}
+
+export default App;
